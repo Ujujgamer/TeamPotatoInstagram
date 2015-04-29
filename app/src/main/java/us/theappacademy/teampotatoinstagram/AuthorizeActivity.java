242d7b0e7161c8eeb1df59908e2c5780dacf1e0c
Bryan Diaz <bryandiaz2010@yahoo.com>
2015-04-29 14:32:47 -0700

    OAuth for Android - 05 Creating Authorization Fragment

--- app/src/main/java/us/theappacademy/teampotatoinstagram/AuthorizeActivity.java
+++ app/src/main/java/us/theappacademy/teampotatoinstagram/AuthorizeActivity.java
@@ -3,14 +3,18 @@ package us.theappacademy.teampotatoinstagram;
 import android.app.Fragment;
 import android.os.Bundle;
 
+import us.theappacademy.oauth.OAuthParameters;
+import us.theappacademy.oauth.util.UrlBuilder;
+import us.theappacademy.oauth.view.AuthorizeFragment;
 import us.theappacademy.oauth.view.OAuthActivity;
 
 public class AuthorizeActivity extends OAuthActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //create an Instagram Connection
+        oauthConnection= new InstagramConnection();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authorize);
     }
 
     @Override
@@ -23,8 +27,24 @@ public class AuthorizeActivity extends OAuthActivity{
 
     }
 
+    //contain multiple fragments for the activity
     @Override
     protected Fragment createFragment() {
-        return null;
+        //provide a way to log in to Instagram on app
+        AuthorizeFragment authorizeFragment= new AuthorizeFragment();
+
+        //in order to make connection to API, check its parameter
+        OAuthParameters oAuthParameters= new OAuthParameters();
+        oAuthParameters.addParameter("client_id", oauthConnection.getClientID());
+        oAuthParameters.addParameter("redirect_uri", oauthConnection.getRedirectUrl());
+        oAuthParameters.addParameter("response_type", "code");
+        oAuthParameters.addParameter("state", UrlBuilder.generateUniqueState(16));
+
+        //check the state and store
+        oauthConnection.state= oAuthParameters.getValueFromParameter("state");
+
+        //get storage from legit user
+        authorizeFragment.setOAuthParameters(oAuthParameters);
+        return authorizeFragment;
     }
 }
