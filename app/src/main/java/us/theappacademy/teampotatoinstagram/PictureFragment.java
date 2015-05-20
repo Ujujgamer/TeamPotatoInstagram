72ff466f1b88ad671a8c678aa8290120fcad8631
mutatedamerican <bryandiaz2010@yahoo.com>
2015-05-20 14:13:45 -0700

    Start of the Game

--- app/src/main/java/us/theappacademy/teampotatoinstagram/PictureFragment.java
+++ app/src/main/java/us/theappacademy/teampotatoinstagram/PictureFragment.java
@@ -1,10 +1,26 @@
 package us.theappacademy.teampotatoinstagram;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import us.theappacademy.oauth.view.OAuthFragment;
 
 public class PictureFragment extends OAuthFragment{
-    @Override
-    public void onTaskFinished(String s) {
 
+    @Override
+    public void onTaskFinished(String string) {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
